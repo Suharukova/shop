c63593937d8fdeb0a61aa401b05288111ef06e3a
Elizabeth Suharukova <liza.s.mail@mail.ru>
2019-05-15 21:24:08 +0300

    Sort & Search

---
+++ shop/catalog/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+
+
+class ItemSortForm(forms.Form):
+    order_by = forms.ChoiceField(choices=[
+        ('name', 'name [A-Z]'),
+        ('-name', 'name [Z-A]'),
+        ('price', 'price'),
+        ('-price', 'price (desc)'),
+        ('count', 'count'),
+    ], label='Order')
+
+
+class ItemFilterForm(forms.Form):
+    name__contains = forms.CharField(max_length=200, required=False, label='Name')
+    price__gt = forms.DecimalField(decimal_places=2, max_digits=15, required=False, label='Price (greater than)')
+    price__lt = forms.DecimalField(decimal_places=2, max_digits=15, required=False, label='Price (less than)')
--- shop/catalog/views.py
+++ shop/catalog/views.py
@@ -3,8 +3,20 @@ from django.shortcuts import render, get_object_or_404
 # Create your views here.
 from catalog.models import Item
 
+from catalog.forms import ItemSortForm, ItemFilterForm
+
+
 def item_list(request):
-    return render(request, 'item_list.html', {'items': Item.objects.all()})
+    item_sort_form = ItemSortForm(request.GET)
+    item_filter_form = ItemFilterForm(request.GET)
+    return render(request, 'item_list.html', {
+        'item_sort_form': item_sort_form,
+        'item_filter_form': item_filter_form,
+        'items': Item.objects.filter(**{k: v for(k, v) in item_filter_form.cleaned_data.items()
+        if v is not None} if item_filter_form.is_valid() else {})
+                                            .order_by(item_sort_form.cleaned_data['order_by']
+                                            if item_sort_form.is_valid() else 'name')
+    })
 
 def item_detail(request, pk):
     return render(request, 'item_detail.html',
