909531762a996d464cd1b619f639b0e953258673
Elizabeth Suharukova <liza.s.mail@mail.ru>
2019-05-30 20:25:02 +0300

    assert in function in_stock

--- shop/catalog/templatetags/catalog_filters.py
+++ shop/catalog/templatetags/catalog_filters.py
@@ -4,5 +4,6 @@ register = Library()
 
 @register.filter(name='in_stock')
 def in_stock(count, arg):
-    arg = arg.split('|')
+    arg = str(arg).split('|')
+    assert len(arg) == 2, "arg must have 2 values"
     return arg[0] if count > 0 else arg[1]
